c76febf27c33b1ac9e7917d99a423055a920ca68
Richard Osborne <romosborne@gmail.com>
2023-06-07 17:21:02 +0100

    Update rooms

--- Models/State.cs
+++ Models/State.cs
@@ -1,25 +1,27 @@
-public class State {
+public class State
+{
     public Room[] Rooms { get; set; }
     public Input[] Inputs { get; set; }
 
-    public State() {
+    public State()
+    {
         Rooms = new Room[]{
-            new Room(1, "Kitchen"),
-            new Room(2, "Butterfly Room"),
+            new Room(1, "Mezzanine above bed"),
+            new Room(2, "Mezzanine by TV"),
             new Room(3, "Courtyard"),
-            new Room(4, "Back Patio"),
-            new Room(5, "Mezzanine"),
-            new Room(6, "Annex"),
-            new Room(7, "Garage"),
-            new Room(8, "Back Bedroom"),
-            new Room(9, "Dog Room"),
-            new Room(10, "Laura's Bedroom"),
-            new Room(11, "Kitchen"),
-            new Room(12, "Kitchen"),
-            new Room(13, "Kitchen"),
-            new Room(14, "Kitchen"),
-            new Room(15, "Kitchen"),
-            new Room(16, "Kitchen")
+            new Room(4, "Back Bedroom"),
+            new Room(5, "Dog room"),
+            new Room(6, "Laura's room"),
+            new Room(7, "Office"),
+            new Room(8, "8 - Unknown"),
+            new Room(9, "Kitchen"),
+            new Room(10, "10 - Unknown"),
+            new Room(11, "11 - Unknown"),
+            new Room(12, "Main Bathroom"),
+            new Room(13, "Back Patio"),
+            new Room(14, "Dining Room"),
+            new Room(15, "15 - Unknown"),
+            new Room(16, "Dining Room")
         };
 
         Inputs = new Input[]{
@@ -29,7 +31,8 @@ public class State {
         };
     }
 
-    public void AdjustRoom(int room, Action<Room> f) {
+    public void AdjustRoom(int room, Action<Room> f)
+    {
         f(Rooms.Single(r => r.Id == room));
     }
 }
